dd930dc5aad2f74c30e2faa48746b6dbca072734
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-04-09 20:51:18 -0400

    yarn update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/sidebar/Sidebar'
 import ItemList from './components/itemList/ItemList';
 
 function App() {
